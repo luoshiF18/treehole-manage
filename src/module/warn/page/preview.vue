69da335080c7c63b6585e99c44e92ef9b5d7f975
yield <3502746856@qq.com>
2019-10-25 18:46:06 +0800

    预警

---
+++ src/module/warn/page/preview.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <h1>查看</h1>
+      <div class="dv">
+        <el-form label-position="right" class="input-width">
+          <el-form-item label="登录名">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="量表名">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="预警等级">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="提交信息">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="提交时间">
+            <el-input></el-input>
+          </el-form-item>
+          <router-link tag="span" to="/warn/page/list">
+            <el-button type="primary">返回</el-button>
+          </router-link>
+        </el-form>
+      </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'preview',
+    }
+</script>
+<style scoped>
+  .input-width{
+    width: 300px;
+  }
+</style>
--- src/module/warn/page/warn_list.vue
+++ src/module/warn/page/warn_list.vue
@@ -2,28 +2,31 @@
     <div>
       <h1>预警列表</h1>
       <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="活动时间">
+      <el-row :gutter="20">
         <el-col :span="4">
           <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
         </el-col>
-        <el-col class="line" :span="1">----</el-col>
+        <el-col class="line" :span="1">-</el-col>
         <el-col :span="4">
           <el-date-picker type="date" placeholder="选择日期" v-model="form.date2" style="width: 100%;"></el-date-picker>
         </el-col>
-      </el-form-item>
-      </el-form>
-
-      <el-form>
         <el-col class="col" :span="4">
           <el-input  placeholder="用户名"></el-input>
         </el-col>
-        <el-col class="col" :span="4">
+        <el-col class="col" :span="3">
           <el-input placeholder="量表名"></el-input>
         </el-col>
-      </el-form>
-
-      <el-form>
-        <el-button type="primary">查询按钮</el-button>
+        <el-col :span="3">
+          <el-select v-model="form.region" placeholder="预警等级">
+            <el-option label="一" value="shanghai"></el-option>
+            <el-option label="二" value="beijing"></el-option>
+            <el-option label="三" value="beijing"></el-option>
+          </el-select>
+        </el-col>
+        <el-col :span="2">
+          <el-button type="primary" size="mini">查询按钮</el-button>
+        </el-col>
+      </el-row>
       </el-form>
 
       <el-table style="width: 100%">
@@ -35,9 +38,11 @@
         <el-table-column prop="address" label="量表名称"></el-table-column>
         <el-table-column prop="address" label="预警等级"></el-table-column>
         <el-table-column prop="address" label="提交时间"></el-table-column>
-        <el-table-column fixed="right" label="操作" width="100">
+        <el-table-column fixed="right" label="操作">
           <template slot-scope="scope">
-            <el-button type="primary" size="mini">编辑</el-button>
+            <router-link tag="span" :to="{path:'/warn/page/preview',query:{}}">
+              <el-button type="primary" size="mini">查看</el-button>
+            </router-link>
             <el-button type="danger" size="mini">删除</el-button>
           </template>
         </el-table-column>
@@ -58,7 +63,8 @@
     }
 </script>
 <style scoped>
-  .col{
-    margin-left: 5px;
+  .el-row {
+    margin-bottom: 20px;
+    margin-left: 20px;
   }
 </style>
