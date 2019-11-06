71b4c5917d09fa814e8881422af20fef028e8123
Helayxy <1562823234@qq.com>
2019-11-06 20:24:14 +0800

    添加了新的js方法

--- src/module/psychologist/api/psychologist.js
+++ src/module/psychologist/api/psychologist.js
@@ -4,12 +4,32 @@ import querystring from 'querystring'
 let sysConfig = require('@/../config/sysConfig')
 let apiUrl = sysConfig.xcApiUrlPre;
 
-//页面查询
-export const psychologist_list = (page, size, params) => {
+//根据条件分页查询心理咨询师简介信息
+export const profile_list = (page, size, params) => {
   //将params对象数据拼装成key/value串
   let queryString = querystring.stringify(params);
   //请求服务端的查询接口
   return http.requestQuickGet(apiUrl + '/psychologist/profile/find/all?' + queryString);
+}
+
+//添加心理咨询师简介信息
+export const profile_add = params => {
+  return http.requestPost(apiUrl + '/psychologist/profile/add', params)
+}
+
+//根据id查询心理咨询师简介信息
+export const profile_get = id => {
+  return http.requestQuickGet(apiUrl + '/psychologist/profile/find/' + id)
+}
+
+//修改提交
+export const profile_edit = (params) => {
+  return http.requestPut(apiUrl + '/psychologist/profile/update', params)
+}
+
+//根据id删除简介信息
+export const profile_del = id => {
+  return http.requestDelete(apiUrl + '/psychologist/profile/del/' + id)
 }
 
 
