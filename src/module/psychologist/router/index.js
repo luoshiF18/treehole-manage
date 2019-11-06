712b95b82970a4c850cb8f9802b98c6584728916
Helayxy <1562823234@qq.com>
2019-11-06 20:26:37 +0800

    添加了新的功能模块

--- src/module/psychologist/router/index.js
+++ src/module/psychologist/router/index.js
@@ -1,5 +1,7 @@
 import Home from '@/module/home/page/home.vue';
-import profile_list from '@/module/psychologist/page/psychologist_list.vue';
+import profile_list from '@/module/psychologist/page/profile_list.vue';
+import profile_add from '@/module/psychologist/page/profile_add.vue';
+import profile_edit from '@/module/psychologist/page/profile_edit.vue';
 
 export default [{
   path: '/profile',
@@ -7,7 +9,9 @@ export default [{
   name: '心理咨询师信息管理',  //菜单名称
   hidden: false,
   children: [
-    {path: '/psychologist/page/list', name: '心理咨询师信息列表', component: profile_list, hidden: false}
+    {path: '/psychologist/profile/list', name: '心理咨询师简介列表', component: profile_list, hidden: false},
+    {path: '/psychologist/profile/add', name: '新增心理咨询师简介信息', component: profile_add, hidden: true},
+    {path: '/psychologist/profile/edit/:id', name: '修改心理咨询师简介信息', component: profile_edit, hidden: true}
   ]
 }
 ]
