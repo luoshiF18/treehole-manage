866885d1329d348063520756ef54efb624c30a98
yield <3502746856@qq.com>
2019-11-07 17:18:22 +0800

    告一段落

--- src/module/scale/page/editDes.vue
+++ src/module/scale/page/editDes.vue
@@ -3,61 +3,136 @@
     <el-button style="margin: 10px" size="small" @click="goback">
       <i class="iconfont icon-fanhui"></i> 返回
     </el-button>
-    <div class="dv-des">
-      <el-card>
-      <el-form label-width="80px">
-        <el-form-item label="描述">
-          <el-input
-            type="textarea"
-            :autosize="{ minRows: 1, maxRows:4}"
-            placeholder="description"
-            resize="none">
-          </el-input>
-        </el-form-item>
-        <el-form-item label="量表ID">
-          <el-input placeholder="scaleId"></el-input>
-        </el-form-item>
-        <el-form-item label="得分1">
-          <el-input placeholder="score1"></el-input>
-        </el-form-item>
-        <el-form-item label="得分2">
-          <el-input placeholder="score2"></el-input>
-        </el-form-item>
-        <el-form-item label="预警等级">
-          <el-input placeholder="warningLevel"></el-input>
-        </el-form-item>
-        <el-form-item label="警告信息">
-          <el-input
-            type="textarea"
-            :autosize="{ minRows: 1, maxRows:4}"
-            placeholder="warningMessage"
-            resize="none">
-          </el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submit">提交</el-button>
-        </el-form-item>
-      </el-form>
-      </el-card>
+    <div class="body-dv">
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-card>
+            <!--stripe是否为斑马纹-->
+            <el-table stripe
+                      :data="desShow">
+              <!-- type="expand" 和 Scoped slot 可以开启展开行功能，
+              el-table-column 的模板会被渲染成为展开行的内容-->
+              <el-table-column align="center"
+                               width="100"
+                               sortable
+                               label="得分1"
+                               prop="score1">
+              </el-table-column>
+              <el-table-column align="center"
+                               width="100"
+                               sortable
+                               label="得分2"
+                               prop="score2">
+              </el-table-column>
+              <!--show-overflow-tooltip当内容过长被隐藏时显示 tooltip-->
+              <el-table-column :show-overflow-tooltip="true"
+                               label="描述信息"
+                               prop="description">
+              </el-table-column>
+              <el-table-column align="center"
+                               width="100"
+                               sortable
+                               label="预警等级"
+                               prop="radio1">
+              </el-table-column>
+              <el-table-column :show-overflow-tooltip="true"
+                               label="警告信息"
+                               prop="warningMessage">
+              </el-table-column>
+            </el-table>
+          </el-card>
+        </el-col>
+
+        <el-col :span="12">
+          <el-card>
+            <el-form :inline="true" :model="form" ref="form">
+              <el-form-item label="得分1" prop="score1">
+                <el-input v-model="form.score1" placeholder="score1"></el-input>
+              </el-form-item>
+              <el-form-item label="得分2" prop="score2">
+                <el-input v-model="form.score2" placeholder="score2"></el-input>
+              </el-form-item>
+
+              <div style="margin-top: 10px"></div>
+              <el-form-item label="描述信息" prop="description">
+                <el-input class="ele-input" type="textarea"
+                          :autosize="{ minRows: 3, maxRows:8}"
+                          placeholder="description"
+                          v-model="form.description"
+                          resize="none"></el-input>
+              </el-form-item>
+
+              <div style="margin-top: 10px"></div>
+              <el-form-item label="预警等级" prop="radio1">
+                <div>
+                  <el-radio-group v-model="form.radio1" size="mini">
+                    <el-radio-button label="1"></el-radio-button>
+                    <el-radio-button label="2"></el-radio-button>
+                    <el-radio-button label="3"></el-radio-button>
+                    <el-radio-button label="4"></el-radio-button>
+                    <el-radio-button label="5"></el-radio-button>
+                  </el-radio-group>
+                </div>
+              </el-form-item>
+              <div style="margin-top: 10px"></div>
+              <el-form-item label="警告信息" prop="warningMessage">
+                <el-input
+                  class="ele-input"
+                  type="textarea"
+                  :autosize="{ minRows: 3, maxRows:8}"
+                  placeholder="warningMessage"
+                  v-model="form.warningMessage"
+                  resize="none">
+                </el-input>
+              </el-form-item>
+              <div style="margin:10px"></div>
+              <el-form-item>
+                <el-button style="margin-left: 50%" type="primary" @click="submit('form')">提交</el-button>
+              </el-form-item>
+            </el-form>
+          </el-card>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
 <script>
   export default {
     name: 'editDes',
+    data() {
+      return {
+        form: {
+          score1: '',
+          score2: '',
+          description: '',
+          radio1: '1',
+          warningMessage: ''
+        },
+        desShow:[]
+      }
+    },
     methods: {
       goback() {
         alert('00');
       },
-      submit(){
-        alert('提交');
-      }
-    }
+      submit(formName) {
+        this.$refs[formName].resetFields();
+      },
+    },
   }
 </script>
 <style scoped>
-  .dv-des {
-    width: 500px;
+  .body-dv {
+
+  }
+
+  .ele-input {
+    width: 25vw;
+  }
+
+  .card-style {
+    margin-top: 10px;
+    /* width: 35vw;*/
   }
 
   .iconfont {
--- src/module/warn/page/preview.vue
+++ src/module/warn/page/preview.vue
@@ -1,37 +1,183 @@
 <template>
-    <div>
-      <h1>查看</h1>
-      <div class="dv">
-        <el-form label-position="right" class="input-width">
-          <el-form-item label="登录名">
-            <el-input></el-input>
-          </el-form-item>
-          <el-form-item label="量表名">
-            <el-input></el-input>
-          </el-form-item>
-          <el-form-item label="预警等级">
-            <el-input></el-input>
-          </el-form-item>
-          <el-form-item label="提交信息">
-            <el-input></el-input>
-          </el-form-item>
-          <el-form-item label="提交时间">
-            <el-input></el-input>
-          </el-form-item>
-          <router-link tag="span" to="/warn/page/list">
-            <el-button type="primary">返回</el-button>
-          </router-link>
-        </el-form>
-      </div>
+  <div>
+    <el-button type="info" size="small" @click="goback"><span class="iconfont icon-fanhui"></span></el-button>
+    <h1>预警报告</h1>
+    <div class="dv">
+      <el-row :gutter="20">
+        <el-col :span="4">
+          <div>
+            <el-upload
+              class="avatar-uploader"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess">
+              <img v-if="imageUrl" :src="imageUrl" class="avatar">
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </div>
+        </el-col>
+        <el-col :span="9">
+          <div>
+            <el-form label-width="90px">
+              <el-form-item label="昵称：">
+                <span>{{tt}}</span>
+              </el-form-item>
+              <el-form-item label="姓名：">
+                <span></span>
+              </el-form-item>
+              <el-form-item label="生日：">
+                <span></span>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-col>
+        <el-col :span="9">
+          <div>
+            <el-form label-width="90px">
+              <el-form-item label="电话号码：">
+                <span></span>
+              </el-form-item>
+              <el-form-item label="邮箱：">
+                <span></span>
+              </el-form-item>
+              <el-form-item label="居住地：">
+                <span></span>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="2">
+          <div class="font-one">测评量表信息</div>
+        </el-col>
+        <el-col :span="20">
+          <div>
+            <el-form label-width="90px">
+              <el-form-item label="量表名称：">
+                <span></span>
+              </el-form-item>
+              <el-form-item label="量表分类：">
+                <span></span>
+              </el-form-item>
+              <el-form-item label="量表描述：">
+               <span></span>
+              </el-form-item>
+              <el-form-item label="量表功能：">
+                <span></span>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="2">
+          <div class="font-two">心理咨询师建议</div>
+        </el-col>
+        <el-col :span="20">
+          <div>
+            <el-form label-width="90px">
+              <el-form-item>
+                <el-input  v-model="textarea"
+                           type="textarea"
+                           placeholder="咨询师预警建议："
+                           resize="none"
+                           :autosize="{ minRows: 8, maxRows: 16}">
+                </el-input>
+              </el-form-item>
+              <el-form-item>
+                <div class="block">
+                  <span class="demonstration"></span>
+                  <el-date-picker
+                    v-model="value1"
+                    type="datetime"
+                    placeholder="选择日期时间">
+                  </el-date-picker>
+                </div>
+              </el-form-item>
+            </el-form>
+          </div>
+        </el-col>
+      </el-row>
     </div>
+    <el-form>
+      <el-form-item>
+        <el-button style="margin-left:36vw" type="primary" @click="save">提交</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 <script>
-    export default {
-        name: 'preview',
+  export default {
+    name: 'preview',
+    data() {
+      return {
+        imageUrl: '',
+        value1: '',
+        textarea:'',
+        tt:'你好'
+      };
+    },
+    methods: {
+      handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      goback(){
+        this.$router.push({path:'/warn/page/list'})
+      },
+      save(){
+        alert('00');
+      }
     }
+  }
 </script>
 <style scoped>
-  .input-width{
-    width: 300px;
+  .avatar-uploader{
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 10vw;
+    height: 10vw;
+    line-height: 10vw;
+    text-align: center;
+  }
+  .avatar {
+    width: 10vw;
+    height: 10vw;
+    display: block;
+  }
+  .el-row {
+    margin-bottom: 20px;
+  &
+  :last-child {
+    margin-bottom: 0;
+  }
+  }
+  .font-one{
+    width: 15px;
+    margin: 38px auto;
+    line-height: 24px;
+    font-size: 20px;
+  }
+  .font-two{
+    width: 15px;
+    margin: 14px auto;
+    line-height: 24px;
+    font-size: 20px;
+  }
+  h1{
+    text-align: center;
+  }
+  .block{
+    float: right;
   }
 </style>
