28d5b3baa902e856ad62e9be1cb275abfc05581b
Helayxy <1562823234@qq.com>
2019-11-11 16:35:20 +0800

    引入状态信息相关页面模块

--- src/module/psychologist/router/index.js
+++ src/module/psychologist/router/index.js
@@ -2,16 +2,22 @@ import Home from '@/module/home/page/home.vue';
 import profile_list from '@/module/psychologist/page/profile_list.vue';
 import profile_add from '@/module/psychologist/page/profile_add.vue';
 import profile_edit from '@/module/psychologist/page/profile_edit.vue';
+import state_list from '@/module/psychologist/page/state_list.vue';
+import state_add from '@/module/psychologist/page/state_add.vue';
+import state_edit from '@/module/psychologist/page/state_edit.vue';
 
 export default [{
-  path: '/profile',
+  path: '/',
   component: Home,
   name: '心理咨询师信息管理',  //菜单名称
   hidden: false,
   children: [
-    {path: '/psychologist/profile/list', name: '心理咨询师简介列表', component: profile_list, hidden: false},
-    {path: '/psychologist/profile/add', name: '新增心理咨询师简介信息', component: profile_add, hidden: true},
-    {path: '/psychologist/profile/edit/:id', name: '修改心理咨询师简介信息', component: profile_edit, hidden: true}
+    {path: '/psychologist/profile/list', name: '简介信息列表', component: profile_list, hidden: false},
+    {path: '/psychologist/profile/add', name: '新增简介信息', component: profile_add, hidden: true},
+    {path: '/psychologist/profile/edit/:id', name: '修改简介信息', component: profile_edit, hidden: true},
+    {path: '/psychologist/state/list', name: '状态信息列表', component: state_list, hidden: false},
+    {path: '/psychologist/state/add', name: '新增状态信息', component: state_add, hidden: true},
+    {path: '/psychologist/state/edit/:id', name: '修改状态信息', component: state_edit, hidden: true},
   ]
 }
 ]
