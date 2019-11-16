ea4cc4bc9ff2273aba067222fa168fa37a119804
Helayxy <1562823234@qq.com>
2019-11-16 20:31:36 +0800

    导入详情信息相关页面

--- src/module/psychologist/router/index.js
+++ src/module/psychologist/router/index.js
@@ -5,6 +5,9 @@ import profile_edit from '@/module/psychologist/page/profile_edit.vue';
 import state_list from '@/module/psychologist/page/state_list.vue';
 import state_add from '@/module/psychologist/page/state_add.vue';
 import state_edit from '@/module/psychologist/page/state_edit.vue';
+import detail_list from '@/module/psychologist/page/detail_list.vue';
+import detail_add from '@/module/psychologist/page/detail_add.vue';
+import detail_edit from '@/module/psychologist/page/detail_edit.vue';
 
 export default [{
   path: '/',
@@ -18,6 +21,9 @@ export default [{
     {path: '/psychologist/state/list', name: '状态信息列表', component: state_list, hidden: false},
     {path: '/psychologist/state/add', name: '新增状态信息', component: state_add, hidden: true},
     {path: '/psychologist/state/edit/:id', name: '修改状态信息', component: state_edit, hidden: true},
+    {path: '/psychologist/detail/list', name: '详情信息列表', component: detail_list, hidden: false},
+    {path: '/psychologist/detail/add', name: '新增详情信息', component: detail_add, hidden: true},
+    {path: '/psychologist/detail/edit/:psychologist_id', name: '修改详情信息', component: detail_edit, hidden: true},
   ]
 }
 ]
