68312cdaac039420569fe8a7b5c7a2674cebeeaf
yield <3502746856@qq.com>
2019-11-21 10:38:09 +0800

    后台登录

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ export default {
 
   data() {
     return {
-      name: 'app'
+      name: 'app',
     }
   },
   methods: {
@@ -23,5 +23,7 @@ export default {
 <style>
   * {
     -webkit-user-select:text;
+    margin: 0;
+    padding: 0;
   }
 </style>
